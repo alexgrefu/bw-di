ed60f078c92070751fecffd9648b48ea3a3bfaf5
Alex Grefu <alex.grefu@jlg.ro>
2020-09-05 18:48:15 +0300

    added basic project structure

---
+++ src/main/java/com/bitwise/bwdi/BwDiApplication.java
@@ -0,0 +1,13 @@
+package com.bitwise.bwdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwDiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BwDiApplication.class, args);
+    }
+
+}
